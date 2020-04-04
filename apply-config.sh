3497d35d64d2e35b0fd4de238369310c6e7e8287
Eirik Falck <eirik.falck@gmail.com>
2020-04-04 17:40:24 +0200

    update apply-config.sh to prompt user for overwrite, append or cancel for vimrc and bashrc. also add prompt for which ps1 to use

--- apply-config.sh
+++ apply-config.sh
@@ -1,49 +1,63 @@
 # Copy vimrc
-if [ -f "$HOME/.vimrc" ]
-then
-    echo ".vimrc exists. Copy manually"
+if [[ -f "$HOME/.vimrc" ]]; then
+    read -p ".vimrc exists. Do you want to [a]ppend, over[w]rite, or [c]ancel? " vimrc_prompt
+    if [[ "$vimrc_prompt" == "a" ]]; then
+        echo "Appending to .vimrc"
+        cat vimrc >> $HOME/.vimrc 
+    elif [[ "$vimrc_prompt" == "w" ]]; then
+        echo "Overwriting .vimrc"
+        cp -f vimrc $HOME/.vimrc
+    fi
 else
+    echo "Copying .vimrc"
     cp vimrc $HOME/.vimrc
 fi
 
-# Copy ps1
-CONFIG_PATH="$HOME/.config"
-if [ ! -d "$CONFIG_PATH" ]
-then
-    echo "Creating $CONFIG_PATH"
-    mkdir -p "$CONFIG_PATH"
-fi
-cp ps1 $CONFIG_PATH/ps1
-cp ps1-ip $CONFIG_PATH/ps1-ip
-
 # Copy bashrc
-if [ -f "$HOME/.bashrc" ]
-then
-    echo ".bashrc exists. Copy manually"
+if [[ -f "$HOME/.bashrc" ]]; then
+    read -p ".bashrc exists. Do you want to [a]ppend, over[w]rite, or [c]ancel? " bashrc_prompt
+    if [[ "$bashrc_prompt" == "a" ]]; then
+        echo "Appending to .bashrc"
+        cat bashrc >> $HOME/.bashrc 
+    elif [[ "$bashrc_prompt" == "w" ]]; then
+        echo "Overwriting .bashrc"
+        cp -f bashrc $HOME/.bashrc
+    fi
 else
+    echo "Copying .bashrc"
     cp bashrc $HOME/.bashrc
 fi
 
+# Create config path
+CONFIG_PATH="$HOME/.config"
+echo "Creating $CONFIG_PATH"
+mkdir -p "$CONFIG_PATH"
+
+# Copy ps1
+cp ps1 $CONFIG_PATH/ps1
+cp ps1-ip $CONFIG_PATH/ps1-ip
+read -p "Use [s]tandard ps1 or [ip] ps1? " ps1_choice
+if [[ "$ps1_choice" == "ip" ]]; then 
+    ps1_choice=ps1-ip
+else
+    ps1_choice=ps1
+fi
+echo "source $CONFIG_PATH/.$ps1_choice" >> $HOME/.bashrc
+
 
 # Create necessary paths for custom programs
-LOCAL_SRC_PATH="$HOME/.local/src"
-LOCAL_BIN_PATH="$HOME/.local/bin"
+LOCAL_SRC_PATH="$CONFIG_PATH/src"
+LOCAL_BIN_PATH="$CONFIG_PATH/bin"
 
-if [ ! -d "$LOCAL_SRC_PATH" ]
-then
-    echo "Creating $LOCAL_SRC_PATH"
-    mkdir -p "$LOCAL_SRC_PATH"
-fi
+echo "Creating $LOCAL_SRC_PATH"
+mkdir -p "$LOCAL_SRC_PATH"
 
-if [ ! -d "$LOCAL_BIN_PATH" ]
-then
-    echo "Creating $LOCAL_BIN_APTH"
-    mkdir -p "$LOCAL_BIN_PATH"
-fi
+echo "Creating $LOCAL_BIN_APTH"
+mkdir -p "$LOCAL_BIN_PATH"
 
 cp -r ./local/* $HOME/.local
-
 
 # Apply git aliases
 chmod +x git-aliases
 ./git-aliases
+
